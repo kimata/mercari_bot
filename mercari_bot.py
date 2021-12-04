9db5bad18bd7b2ee1a2a6a10862489b398bae1b4
KIMATA Tetsuya <kimata@green-rabbit.net>
2021-12-04 16:02:37 +0900

    fix: パスを絶対パスで指定するように変更

--- mercari_bot.py
+++ mercari_bot.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python3
 # - coding: utf-8 --
+import chromedriver_binary
+
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.wait import WebDriverWait
@@ -15,12 +17,19 @@ import random
 
 import yaml
 import pprint
+import pathlib
+
 
 LOGIN_URL = 'https://jp.mercari.com'
 CONFIG_PATH = 'config.yml'
 CHROME_DATA_PATH = 'chrome_data'
 PRICE_DOWN_STEP = 10
 PRICE_THRESHOLD = 5000
+
+
+def get_abs_path(path):
+    return str(pathlib.Path(os.path.dirname(__file__), path))
+
 
 def error(message):
     click.secho('ERROR: ', fg='red', bold=True, nl=False)
@@ -34,7 +43,7 @@ def info(message):
 
 
 def load_config():
-    with open(CONFIG_PATH) as file:
+    with open(get_abs_path(CONFIG_PATH)) as file:
         return yaml.safe_load(file)
 
 
@@ -81,12 +90,13 @@ def login(driver, wait, config):
     wait.until(EC.element_to_be_clickable((By.XPATH, '//mer-text[contains(text(), "アカウント")]')))
 
 
+
 def create_driver():
     options = Options()
     options.add_argument('--headless')
     options.add_argument('--lang=ja-JP')
     options.add_argument('--window-size=1920,1024')
-    options.add_argument('--user-data-dir=' + CHROME_DATA_PATH)
+    options.add_argument('--user-data-dir=' + get_abs_path(CHROME_DATA_PATH))
 
     driver = webdriver.Chrome(options=options)
 
@@ -120,12 +130,13 @@ def iter_items_on_display(driver, wait, item_func):
         wait.until(EC.title_contains(name))
         item_func(driver, wait, name, price)
 
-        time.sleep(4 + (6*random()))
+        time.sleep(4 + (6*random.random()))
         driver.get(list_url)
         wait.until(EC.presence_of_element_located((By.XPATH, '//mer-list[@data-testid="listed-item-list"]/mer-list-item')))
 
+    info('完了')
 
-    
+
 def item_price_down(driver, wait, name, price):
     if (price < PRICE_THRESHOLD):
         print('  現在価格が{:,}円のため，スキップします．'.format(price))
@@ -166,3 +177,5 @@ login(driver, wait, config)
 iter_items_on_display(driver, wait, item_price_down)
 
 driver.quit()
+
+sys.exit(0)
