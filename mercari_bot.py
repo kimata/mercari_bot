9ae828e059f1596e0a924c4de379a22870337f7f
KIMATA Tetsuya <kimata@green-rabbit.net>
2021-12-20 19:43:23 +0900

    fix: black で整形

--- mercari_bot.py
+++ mercari_bot.py
@@ -19,9 +19,9 @@ import pprint
 import pathlib
 
 
-LOGIN_URL = 'https://jp.mercari.com'
-CONFIG_PATH = 'config.yml'
-CHROME_DATA_PATH = 'chrome_data'
+LOGIN_URL = "https://jp.mercari.com"
+CONFIG_PATH = "config.yml"
+CHROME_DATA_PATH = "chrome_data"
 PRICE_DOWN_STEP = 100
 PRICE_THRESHOLD = 3000
 
@@ -31,13 +31,13 @@ def get_abs_path(path):
 
 
 def error(message):
-    click.secho('ERROR: ', fg='red', bold=True, nl=False)
+    click.secho("ERROR: ", fg="red", bold=True, nl=False)
     click.secho(message)
     sys.exit(-1)
 
 
 def info(message):
-    click.secho('INFO: ', fg='white', bold=True, nl=False)
+    click.secho("INFO: ", fg="white", bold=True, nl=False)
     click.secho(message)
 
 
@@ -53,17 +53,20 @@ def click_xpath(driver, xpath, wait=None):
 
 
 def expand_shadow_element(driver, element):
-    shadow_root = driver.execute_script('return arguments[0].shadowRoot', element)
+    shadow_root = driver.execute_script("return arguments[0].shadowRoot", element)
     return shadow_root
-    
-    
+
+
 def login(driver, wait, config):
     driver.get(LOGIN_URL)
-    time.sleep(1) # NOTE: これを削ると NG になる...
-    wait.until(lambda x:
-               x.find_elements_by_xpath('//mer-text[contains(text(), "ログイン")]') or
-               x.find_elements_by_xpath('//mer-text[contains(text(), "アカウント")]') or
-               x.find_elements_by_xpath('//button[contains(text(), "はじめる")]'))
+
+    time.sleep(1)  # NOTE: これを削ると NG になる...
+
+    wait.until(
+        lambda x: x.find_elements_by_xpath('//mer-text[contains(text(), "ログイン")]')
+        or x.find_elements_by_xpath('//mer-text[contains(text(), "アカウント")]')
+        or x.find_elements_by_xpath('//button[contains(text(), "はじめる")]')
+    )
 
     if len(driver.find_elements_by_xpath('//button[contains(text(), "はじめる")]')) != 0:
         click_xpath(driver, '//button[contains(text(), "はじめる")]')
@@ -75,29 +78,38 @@ def login(driver, wait, config):
     click_xpath(driver, '//mer-text[contains(text(), "ログイン")]', wait)
     click_xpath(driver, '//span[contains(text(), "メールアドレスでログイン")]', wait)
 
-    wait.until(EC.presence_of_element_located((By.XPATH, '//mer-heading[@title-label="ログイン"]')))
+    wait.until(
+        EC.presence_of_element_located((By.XPATH, '//mer-heading[@title-label="ログイン"]'))
+    )
 
-    driver.find_element_by_xpath('//input[@name="email"]').send_keys(config['user'])
-    driver.find_element_by_xpath('//input[@name="password"]').send_keys(config['pass'])
+    driver.find_element_by_xpath('//input[@name="email"]').send_keys(config["user"])
+    driver.find_element_by_xpath('//input[@name="password"]').send_keys(config["pass"])
     click_xpath(driver, '//button[contains(text(), "ログイン")]', wait)
 
-    wait.until(EC.presence_of_element_located((By.XPATH, '//mer-heading[@title-label="電話番号の確認"]')))
+    wait.until(
+        EC.presence_of_element_located(
+            (By.XPATH, '//mer-heading[@title-label="電話番号の確認"]')
+        )
+    )
 
-    code = input('認証番号: ')
+    code = input("認証番号: ")
     driver.find_element_by_xpath('//input[@name="code"]').send_keys(code)
     click_xpath(driver, '//button[contains(text(), "認証して完了する")]', wait)
 
-    wait.until(EC.element_to_be_clickable((By.XPATH, '//mer-text[contains(text(), "アカウント")]')))
-
+    wait.until(
+        EC.element_to_be_clickable((By.XPATH, '//mer-text[contains(text(), "アカウント")]'))
+    )
 
 
 def create_driver():
     options = Options()
-    options.add_argument('--headless')
-    options.add_argument('--lang=ja-JP')
-    options.add_argument('--window-size=1920,1080')
-    options.add_argument('--user-agent="Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/96.0.4664.93 Safari/537.36"')
-    options.add_argument('--user-data-dir=' + get_abs_path(CHROME_DATA_PATH))
+    options.add_argument("--headless")
+    options.add_argument("--lang=ja-JP")
+    options.add_argument("--window-size=1920,1080")
+    options.add_argument(
+        '--user-agent="Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/96.0.4664.93 Safari/537.36"'
+    )
+    options.add_argument("--user-data-dir=" + get_abs_path(CHROME_DATA_PATH))
 
     driver = webdriver.Chrome(options=options)
 
@@ -108,79 +120,117 @@ def iter_items_on_display(driver, wait, item_func):
     click_xpath(driver, '//mer-text[contains(text(), "アカウント")]')
     click_xpath(driver, '//a[contains(text(), "出品した商品")]', wait)
 
-    wait.until(EC.presence_of_element_located((By.XPATH, '//mer-list[@data-testid="listed-item-list"]/mer-list-item')))
+    wait.until(
+        EC.presence_of_element_located(
+            (By.XPATH, '//mer-list[@data-testid="listed-item-list"]/mer-list-item')
+        )
+    )
 
-    item_count = len(driver.find_elements_by_xpath('//mer-list[@data-testid="listed-item-list"]/mer-list-item'))
+    item_count = len(
+        driver.find_elements_by_xpath(
+            '//mer-list[@data-testid="listed-item-list"]/mer-list-item'
+        )
+    )
 
-    info('{}個の出品があります'.format(item_count))
+    info("{}個の出品があります".format(item_count))
 
     list_url = driver.current_url
     for i in range(1, item_count):
         item_root = expand_shadow_element(
             driver,
-            driver.find_element_by_xpath('//mer-list[@data-testid="listed-item-list"]/mer-list-item[' + str(i) + ']//mer-item-object'))
+            driver.find_element_by_xpath(
+                '//mer-list[@data-testid="listed-item-list"]/mer-list-item['
+                + str(i)
+                + "]//mer-item-object"
+            ),
+        )
 
+        name = item_root.find_element_by_css_selector("div.container").get_attribute(
+            "aria-label"
+        )
+        price = int(
+            item_root.find_element_by_css_selector("mer-price").get_attribute("value")
+        )
 
-        name = item_root.find_element_by_css_selector('div.container').get_attribute('aria-label')
-        price = int(item_root.find_element_by_css_selector('mer-price').get_attribute('value'))
-
-        click.secho('* ', fg='green', bold=True, nl=False)
+        click.secho("* ", fg="green", bold=True, nl=False)
         click.secho(name)
 
-        driver.find_element_by_xpath('//mer-list[@data-testid="listed-item-list"]/mer-list-item[' + str(i) + ']//a').click()
+        driver.find_element_by_xpath(
+            '//mer-list[@data-testid="listed-item-list"]/mer-list-item['
+            + str(i)
+            + "]//a"
+        ).click()
         wait.until(EC.title_contains(name))
         item_func(driver, wait, name, price)
 
-        time.sleep(4 + (6*random.random()))
+        time.sleep(4 + (6 * random.random()))
         driver.get(list_url)
-        wait.until(EC.presence_of_element_located((By.XPATH, '//mer-list[@data-testid="listed-item-list"]/mer-list-item')))
+        wait.until(
+            EC.presence_of_element_located(
+                (By.XPATH, '//mer-list[@data-testid="listed-item-list"]/mer-list-item')
+            )
+        )
 
-    info('完了')
+    info("完了")
 
 
-def item_price_down(driver, wait, name, price):
-    if (price < PRICE_THRESHOLD):
-        print('  現在価格が{:,}円のため，スキップします．'.format(price))
+def item_price_down(driver, wait, name, total_price):
+    if total_price < PRICE_THRESHOLD:
+        print("  現在価格が{:,}円のため，スキップします．".format(total_price))
         return
 
     click_xpath(driver, '//mer-button[@data-testid="checkout-button"]')
-    wait.until(EC.title_contains('商品の情報を編集'))
+    wait.until(EC.title_contains("商品の情報を編集"))
     wait.until(EC.presence_of_element_located((By.XPATH, '//input[@name="price"]')))
 
-    if len(driver.find_elements_by_xpath('//mer-price[@data-testid="shipping-fee"]')) != 0:
-        shipping_fee = int(driver.find_element_by_xpath('//mer-price[@data-testid="shipping-fee"]').get_attribute('value'))
+    # NOTE: 梱包・発送たのメル便の場合は送料を取得
+    if (
+        len(driver.find_elements_by_xpath('//mer-price[@data-testid="shipping-fee"]'))
+        != 0
+    ):
+        shipping_fee = int(
+            driver.find_element_by_xpath(
+                '//mer-price[@data-testid="shipping-fee"]'
+            ).get_attribute("value")
+        )
     else:
         shipping_fee = 0
 
-    if (price - shipping_fee) < PRICE_THRESHOLD:
-        print('  現在価格が{:,}円 (送料: {:,}円) のため，スキップします．'.format(price, shipping_fee))
+    price = total_price - shipping_fee
+
+    if price < PRICE_THRESHOLD:
+        print("  現在価格が{:,}円 (送料: {:,}円) のため，スキップします．".format(price, shipping_fee))
         return
 
-    cur_price = int(driver.find_element_by_xpath('//input[@name="price"]').get_attribute('value'))
-    if (cur_price+shipping_fee) != price:
-        error('ページ遷移中に価格が変更されました．')
+    cur_price = int(
+        driver.find_element_by_xpath('//input[@name="price"]').get_attribute("value")
+    )
+    if cur_price != price:
+        error("ページ遷移中に価格が変更されました．")
 
-    new_price = int((price - shipping_fee - PRICE_DOWN_STEP) / 10) * 10 # 10円単位に丸める
-    driver.find_element_by_xpath('//input[@name="price"]').send_keys(Keys.CONTROL + 'a')
+    new_price = int((price - PRICE_DOWN_STEP) / 10) * 10  # 10円単位に丸める
+    driver.find_element_by_xpath('//input[@name="price"]').send_keys(Keys.CONTROL + "a")
     driver.find_element_by_xpath('//input[@name="price"]').send_keys(Keys.BACK_SPACE)
     driver.find_element_by_xpath('//input[@name="price"]').send_keys(new_price)
     click_xpath(driver, '//button[contains(text(), "変更する")]')
 
     wait.until(EC.title_contains(name))
 
-    wait.until(EC.presence_of_element_located((By.XPATH, '//mer-price')))
+    wait.until(EC.presence_of_element_located((By.XPATH, "//mer-price")))
 
-    cur_price = int(driver.find_element_by_xpath('//mer-price').get_attribute('value'))
+    new_total_price = int(
+        driver.find_element_by_xpath("//mer-price").get_attribute("value")
+    )
 
-    if (cur_price - shipping_fee) != new_price:
-        error('編集後の価格が意図したものと異なっています．')
+    if new_total_price != (new_price + shipping_fee):
+        error("編集後の価格が意図したものと異なっています．")
 
-    print('  {:,}円 -> {:,}円'.format(price, cur_price))
+    print("  {:,}円 -> {:,}円".format(total_price, new_total_price))
 
 
 # NOTE: 端末から実行していない場合は，動き始める前にランダムな時間待つ
 if not sys.stdin.isatty():
-    time.sleep(600*random.random())
+    time.sleep(600 * random.random())
 
 config = load_config()
 driver = create_driver()
