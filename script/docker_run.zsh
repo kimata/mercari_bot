95517f8d93b96aab66fe239bef6663c43b2866d1
KIMATA Tetsuya <kimata@green-rabbit.net>
2022-07-06 23:52:39 +0900

    feat: log と debug ディレクトリもバインドマウントするようにした

--- script/docker_run.zsh
+++ script/docker_run.zsh
@@ -7,4 +7,8 @@ set -e
 cd $(dirname $(dirname $0))
 
 docker build --quiet . -t ${APP_NAME}
-docker run --mount type=bind,source=$(dirname $(cd $(dirname $0); pwd))/chrome_data,target=/opt/mercari_bot/chrome_data --rm ${APP_NAME}
+docker run \
+       --mount type=bind,source=$(dirname $(cd $(dirname $0); pwd))/chrome_data,target=/opt/mercari_bot/chrome_data \
+       --mount type=bind,source=$(dirname $(cd $(dirname $0); pwd))/log,target=/opt/mercari_bot/log \
+       --mount type=bind,source=$(dirname $(cd $(dirname $0); pwd))/debug,target=/opt/mercari_bot/debug \
+       --rm ${APP_NAME}
