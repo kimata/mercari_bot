0cc0d067475efcac4c29e25bae738e220a6a6019
KIMATA Tetsuya <kimata@green-rabbit.net>
2022-07-06 23:49:06 +0900

    feat: chrome_data をバインドマウントするようにした

--- script/docker_run.zsh
+++ script/docker_run.zsh
@@ -7,4 +7,4 @@ set -e
 cd $(dirname $(dirname $0))
 
 docker build --quiet . -t ${APP_NAME}
-docker run --rm ${APP_NAME}
+docker run --mount type=bind,source=$(dirname $(cd $(dirname $0); pwd))/chrome_data,target=/opt/mercari_bot/chrome_data --rm ${APP_NAME}
