dd387c438837cc5736afbc5559718fb2b2b04785
KIMATA Tetsuya <kimata@green-rabbit.net>
2022-07-10 12:11:33 +0900

    fix: コードを整理

--- mercari_bot.py
+++ mercari_bot.py
@@ -13,7 +13,6 @@ from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.keys import Keys
 
-
 from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
 from webdriver_manager.core.utils import ChromeType
@@ -182,7 +181,9 @@ def login(driver, wait, config):
 def create_driver():
     options = Options()
     options.add_argument("--headless")
-    options.add_argument("--no-sandbox")
+    options.add_argument("--no-sandbox")  # for Docker
+    options.add_argument("--disable-dev-shm-usage")  # for Docker
+
     options.add_argument("--lang=ja-JP")
     options.add_argument("--window-size=1920,1080")
 
--- script/docker_run.zsh
+++ script/docker_run.zsh
@@ -8,7 +8,8 @@ cd $(dirname $(dirname $0))
 
 docker build --quiet . -t ${APP_NAME}
 docker run \
-       --mount type=bind,source=$(dirname $(cd $(dirname $0); pwd))/chrome_data,target=/opt/mercari_bot/chrome_data \
-       --mount type=bind,source=$(dirname $(cd $(dirname $0); pwd))/log,target=/opt/mercari_bot/log \
-       --mount type=bind,source=$(dirname $(cd $(dirname $0); pwd))/debug,target=/opt/mercari_bot/debug \
+       --mount type=bind,source=$(dirname $(cd $(dirname $0); pwd))/chrome_data,target=/opt/${APP_NAME}/chrome_data \
+       --mount type=bind,source=$(dirname $(cd $(dirname $0); pwd))/log,target=/opt/${APP_NAME}/log \
+       --mount type=bind,source=$(dirname $(cd $(dirname $0); pwd))/debug,target=/opt/${APP_NAME}/debug \
+       --name ${APP_NAME}-$(head -c 10 /dev/urandom | base64 | tr -dc 'a-zA-Z0-9' | cut -c 1-4) \
        --rm ${APP_NAME}
