2e0d4c3d1f24f38ea6fb0d263fdf87185a59fc0a
KIMATA Tetsuya <kimata@green-rabbit.net>
2023-07-30 23:01:10 +0900

    fix: Uset-Agent を更新

--- src/selenium_util.py
+++ src/selenium_util.py
@@ -41,7 +41,7 @@ def create_driver_impl(profile_name, data_path):
     options.add_argument("--window-size=1920,1080")
 
     options.add_argument(
-        '--user-agent="Mozilla/5.0 (Macintosh; Intel Mac OS X 10.15; rv:108.0) Gecko/20100101 Firefox/108.0"'
+        '--user-agent="Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Safari/537.36"'
     )
     options.add_argument("--user-data-dir=" + str(chrome_data_path / profile_name))
 
