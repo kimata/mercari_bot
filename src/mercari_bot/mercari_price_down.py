9f4105534cb470150dd57b8080341bb1c69c2b09
KIMATA Tetsuya <kimata@green-rabbit.net>
2025-05-04 22:55:25 +0900

    fix: リファクタリング

--- src/mercari_bot/mercari_price_down.py
+++ src/mercari_bot/mercari_price_down.py
@@ -1,5 +1,4 @@
 #!/usr/bin/env python3
-# -*- coding: utf-8 -*-
 import io
 import logging
 import logging.handlers
@@ -25,12 +24,10 @@ def get_modified_hour(driver):
     modified_text = driver.find_element(
         selenium.webdriver.common.by.By.XPATH,
         '//div[@id="item-info"]//div[contains(@class,"merShowMore")]'
-        + '/following-sibling::p[contains(@class, "merText")]',
+        '/following-sibling::p[contains(@class, "merText")]',
     ).text
 
-    if re.compile(r"秒前").search(modified_text):
-        return 0
-    elif re.compile(r"分前").search(modified_text):
+    if re.compile(r"秒前").search(modified_text) or re.compile(r"分前").search(modified_text):
         return 0
     elif re.compile(r"時間前").search(modified_text):
         return int("".join(filter(str.isdigit, modified_text)))
@@ -42,38 +39,31 @@ def get_modified_hour(driver):
         return -1
 
 
-def get_discount_step(profile, price, shipping_fee, favorite_count):
-    for discount_info in reversed(
-        sorted(profile["discount"], key=lambda x: x["favorite_count"])
-    ):
+def get_discount_step(scrape_config, price, shipping_fee, favorite_count):
+    for discount_info in sorted(scrape_config["discount"], key=lambda x: x["favorite_count"], reverse=True):
         if favorite_count >= discount_info["favorite_count"]:
             if price >= discount_info["threshold"]:
                 return discount_info["step"]
             else:
                 logging.info(
-                    "現在価格が{price:,}円 (送料: {shipping:,}円) のため，スキップします．".format(
-                        price=price, shipping=shipping_fee
-                    )
+                    "現在価格が%s円 (送料: %s円) のため，スキップします．", f"{price:,}", f"{shipping_fee:,}"
                 )
+
                 return None
 
-    logging.info(
-        "イイねの数({favorite_count})が条件を満たさなかったので，スキップします．".format(
-            favorite_count=favorite_count
-        )
-    )
+    logging.info("イイねの数(%d)が条件を満たさなかったので，スキップします．", favorite_count)
     return None
 
 
-def execute_item(driver, wait, profile, item, debug_mode):
+def execute_item(driver, wait, scrape_config, item, debug_mode):
     if item["is_stop"] != 0:
         logging.info("公開停止中のため，スキップします．")
         return
 
     modified_hour = get_modified_hour(driver)
 
-    if modified_hour < profile["interval"]["hour"]:
-        logging.info("更新してから {hour} 時間しか経過していないため，スキップします．".format(hour=modified_hour))
+    if modified_hour < scrape_config["interval"]["hour"]:
+        logging.info("更新してから %d 時間しか経過していないため，スキップします．", modified_hour)
         return
 
     favorite_text = driver.find_element(
@@ -81,16 +71,11 @@ def execute_item(driver, wait, profile, item, debug_mode):
         '//div[@data-testid="icon-heart-button"]/button',
     ).get_attribute("aria-label")
 
-    if re.search(r"\d+", favorite_text):
-        favorite_count = int(favorite_text)
-    else:
-        favorite_count = 0
+    favorite_count = int(favorite_text) if re.search("\\d+", favorite_text) else 0
 
     my_lib.selenium_util.click_xpath(driver, '//div[@data-testid="checkout-button"]')
 
-    wait.until(
-        selenium.webdriver.support.expected_conditions.title_contains("商品の情報を編集")
-    )
+    wait.until(selenium.webdriver.support.expected_conditions.title_contains("商品の情報を編集"))
 
     # NOTE: 食品などの場合，「出品情報の確認」の表示が出るので，「OK」ボタンを押す
     if (
@@ -133,45 +118,36 @@ def execute_item(driver, wait, profile, item, debug_mode):
     price = item["price"] - shipping_fee
 
     cur_price = int(
-        driver.find_element(
-            selenium.webdriver.common.by.By.XPATH, '//input[@name="price"]'
-        ).get_attribute("value")
+        driver.find_element(selenium.webdriver.common.by.By.XPATH, '//input[@name="price"]').get_attribute(
+            "value"
+        )
     )
     if cur_price != price:
-        raise RuntimeError("ページ遷移中に価格が変更されました．")
+        raise RuntimeError("ページ遷移中に価格が変更されました．")  # noqa: EM101
 
-    discount_step = get_discount_step(profile, price, shipping_fee, favorite_count)
+    discount_step = get_discount_step(scrape_config, price, shipping_fee, favorite_count)
     if discount_step is None:
         return
 
-    if debug_mode:
-        new_price = price
-    else:
-        new_price = int((price - discount_step) / 10) * 10  # 10円単位に丸める
+    new_price = price if debug_mode else int((price - discount_step) / 10) * 10  # 10円単位に丸める
 
-    driver.find_element(
-        selenium.webdriver.common.by.By.XPATH, '//input[@name="price"]'
-    ).send_keys(selenium.webdriver.common.keys.Keys.CONTROL + "a")
-    driver.find_element(
-        selenium.webdriver.common.by.By.XPATH, '//input[@name="price"]'
-    ).send_keys(selenium.webdriver.common.keys.Keys.BACK_SPACE)
-    driver.find_element(
-        selenium.webdriver.common.by.By.XPATH, '//input[@name="price"]'
-    ).send_keys(new_price)
+    driver.find_element(selenium.webdriver.common.by.By.XPATH, '//input[@name="price"]').send_keys(
+        selenium.webdriver.common.keys.Keys.CONTROL + "a"
+    )
+    driver.find_element(selenium.webdriver.common.by.By.XPATH, '//input[@name="price"]').send_keys(
+        selenium.webdriver.common.keys.Keys.BACK_SPACE
+    )
+    driver.find_element(selenium.webdriver.common.by.By.XPATH, '//input[@name="price"]').send_keys(new_price)
     my_lib.selenium_util.random_sleep(2)
     my_lib.selenium_util.click_xpath(driver, '//button[contains(text(), "変更する")]')
 
     time.sleep(1)
-    my_lib.selenium_util.click_xpath(
-        driver, '//button[contains(text(), "このまま出品する")]', is_warn=False
-    )
+    my_lib.selenium_util.click_xpath(driver, '//button[contains(text(), "このまま出品する")]', is_warn=False)
 
     my_lib.selenium_util.wait_patiently(
         driver,
         wait,
-        selenium.webdriver.support.expected_conditions.title_contains(
-            re.sub(" +", " ", item["name"])
-        ),
+        selenium.webdriver.support.expected_conditions.title_contains(re.sub(" +", " ", item["name"])),
     )
     my_lib.selenium_util.wait_patiently(
         driver,
@@ -202,17 +178,13 @@ def execute_item(driver, wait, profile, item, debug_mode):
     )
 
     if new_total_price != (new_price + shipping_fee):
-        raise RuntimeError(
-            "編集後の価格が意図したものと異なっています．(期待値: {exp:,}円, 実際: {act:,}円)".format(
-                exp=new_price + shipping_fee, act=new_total_price
-            )
+        error_message = (
+            f"編集後の価格が意図したものと異なっています．"
+            f"(期待値: {new_price + shipping_fee:,}円, 実際: {new_total_price:,}円)"
         )
+        raise RuntimeError(error_message)
 
-    logging.info(
-        "価格を変更しました．({total:,}円 -> {new_total:,}円)".format(
-            total=item["price"], new_total=new_total_price
-        )
-    )
+    logging.info("価格を変更しました．(%s円 -> %s円)", f"{item['price']:,}", f"{new_total_price:,}")
 
 
 def execute(config, profile, data_path, dump_path, debug_mode):
@@ -224,26 +196,23 @@ def execute(config, profile, data_path, dump_path, debug_mode):
 
     try:
         my_lib.store.mercari.login.execute(
-            config,
             driver,
             wait,
             profile["line"]["user"],
             profile["line"]["pass"],
+            config["slack"],
             dump_path,
         )
 
-        my_lib.store.mercari.scrape.iter_items_on_display(
-            driver, wait, profile, debug_mode, [execute_item]
-        )
+        my_lib.store.mercari.scrape.iter_items_on_display(driver, wait, profile, debug_mode, [execute_item])
 
         my_lib.selenium_util.log_memory_usage(driver)
 
         return 0
     except Exception:
-        logging.error("URL: {url}".format(url=driver.current_url))
-        logging.error(traceback.format_exc())
+        logging.exception("URL: %s", driver.current_url)
 
-        my_lib.selenium_util.dump_page(driver, int(random.random() * 100), dump_path)
+        my_lib.selenium_util.dump_page(driver, int(random.random() * 100), dump_path)  # noqa: S311
         my_lib.selenium_util.clean_dump(dump_path)
 
         if "slack" in config:
@@ -254,9 +223,7 @@ def execute(config, profile, data_path, dump_path, debug_mode):
                 config["slack"]["from"],
                 traceback.format_exc(),
                 {
-                    "data": PIL.Image.open(
-                        (io.BytesIO(driver.get_screenshot_as_png()))
-                    ),
+                    "data": PIL.Image.open(io.BytesIO(driver.get_screenshot_as_png())),
                     "text": "エラー時のスクリーンショット",
                 },
                 interval_min=config["slack"]["error"]["interval_min"],
